3d985d1092daeed862fcd55ee219b8c733616f7b
Aerton <aertoncastro@gmail.com>
2022-03-08 15:19:22 -0300

    :sparkles: Export Anchor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { Anchor } from "./Anchor";
 import { Engine, EngineOptions } from "./Engine";
 import { GameObject, GameObjectOptions } from "./GameObject";
 import { Vector } from "./Vector";
 
-export {Engine, EngineOptions, GameObject, GameObjectOptions, Vector};
+export {Engine, EngineOptions, GameObject, GameObjectOptions, Vector, Anchor};
