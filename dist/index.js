2b81c907d9569208e0f4a5c0bfad76a77471519f
Aerton <aertoncastro@gmail.com>
2022-03-09 08:42:56 -0300

    :ambulance: PIXI import

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ import { Anchor } from "./Anchor";
 import { Engine } from "./Engine";
 import { GameObject } from "./GameObject";
 import { Vector } from "./Vector";
-import PIXI from 'pixi.js';
+import * as PIXI from 'pixi.js';
 export { Engine, GameObject, Vector, Anchor, PIXI };
