91daa2e0c225005c2b85cc11a97c7c1d7e2278f6
Aerton <aertoncastro@gmail.com>
2022-03-19 01:03:44 -0300

    :construction: consistency

--- src/audio/Audio.ts
+++ src/audio/Audio.ts
@@ -1,16 +1,16 @@
 export type AudioOptions = {
     loop?: boolean;
     volume?: number;
-    autoPlay?: boolean;
+    autoplay?: boolean;
 }
 export class Audio extends HTMLAudioElement{
     constructor(url: string, audioOptions?: AudioOptions){
         super();
 
-        const {loop, volume, autoPlay} = audioOptions;
+        const {loop, volume, autoplay} = audioOptions;
 
         this.src = url;
-        this.autoplay = autoPlay;
+        this.autoplay = autoplay;
         this.loop = loop;
         this.volume = volume ? volume : 1;
         this.style.display = 'none';
