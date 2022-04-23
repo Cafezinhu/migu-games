459d6b92dfdd4bfb9c556cb32e746ff6e3b10f72
Aerton <aertoncastro@gmail.com>
2022-04-23 14:25:38 -0300

    :sparkles: Vector.One(), Vector.Zero(), Vector.Left()... etc

--- src/Vector.ts
+++ src/Vector.ts
@@ -1,4 +1,12 @@
 import Victor from "victor";
 
 export type Vector = Victor;
-export const Vector = Victor;
+export const Vector = {
+    ...Victor,
+    Zero: () => new Victor(0, 0),
+    One: () => new Victor(1, 1),
+    Left: () => new Victor(-1, 0),
+    Right: () => new Victor(1, 0),
+    Up: () => new Victor(0, -1),
+    Down: () => new Victor(0, 1)
+};
