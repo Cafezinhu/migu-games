5fe8f0d401ad4ab39b2f09bb3d307f0946a85513
aerton <aertoncastro@gmail.com>
2022-06-27 11:35:18 -0300

    import sprites cli

---
+++ bin/import-sprites.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import recursive from 'recursive-readdir';
+
+const input = process.argv[2];
+const output = process.argv[3];
+const files = recursive(input, (err, files) => {
+    let sprites = {};
+    files.forEach(file => {
+        if(!file.endsWith('.png')  
+            && !file.endsWith('.jpg')
+            && !file.endsWith('.jpeg')
+            && !file.endsWith('.gif')
+            && !file.endsWith('.bmp')
+            && !file.endsWith('.webp')) return;
+
+        let name = file.replace(`${input}\\`, '').toLowerCase()
+            .replace('.png', '')
+            .replace('.jpg', '')
+            .replace('.jpeg', '')
+            .replace('.gif', '')
+            .replace('.bmp', '')
+            .replace('.webp', '');
+        let nextCap = false;
+        let finalName = '';
+        for(let char of name.split('')){
+            if(char == '\\'){
+                nextCap = true;
+            }else{
+                if(nextCap){
+                    nextCap = false;
+                    finalName = `${finalName}${char.toUpperCase()}`;
+                }else{
+                    finalName = `${finalName}${char}`;
+                }
+            }
+        }
+        sprites[finalName] = file;
+    });
+    
+    fs.writeFileSync(output, `export const sprites = ${JSON.stringify(sprites)}`);
+});
